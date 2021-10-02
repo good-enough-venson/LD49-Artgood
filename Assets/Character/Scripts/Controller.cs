8b582e7de7dd585590809590944d4906cab065bc
Ephraim Goodenough <ephraim.goodenough@neuromatics.net>
2021-10-02 15:57:39 -0500

    worked on test scene and TMPro Essentials

--- Assets/Character/Scripts/Controller.cs
+++ Assets/Character/Scripts/Controller.cs
@@ -1,18 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Controller : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    public float maxAngularVelocity = 100;
+
+    public float torqueForce = 1;
+
+    private Rigidbody rb;
+    public Rigidbody rigidbody { 
+        get { if (rb == null) rb = transform.GetComponent<Rigidbody>(); return rb; } 
+        set => rb = value;    
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        var torque = rigidbody.angularVelocity;
+
+        if (Input.GetKey(KeyCode.LeftArrow) && torque.z > -maxAngularVelocity) {
+            rigidbody.AddRelativeTorque(-Vector3.Forward * torqueForce * Time.FixedDelta, ForceMode.Force);
+        }
+        if (Input.GetKey(KeyCode.RightArrow) && torque.z < maxAngularVelocity) {
+            rigidbody.AddRelativeTorque(Vector3.Forward * torqueForce * Time.FixedDelta, ForceMode.Force);
+        }
+        if (Input.GetKey(KeyCode.DownArrow) && torque.x > -maxAngularVelocity) {
+            rigidbody.AddRelativeTorque(Vector3.Right * torqueForce * Time.FixedDelta, ForceMode.Force);
+        }
+        if (Input.GetKey(KeyCode.UpArrow) && torque.x < maxAngularVelocity) {
+            rigidbody.AddRelativeTorque(Vector3.Right * torqueForce * Time.FixedDelta, ForceMode.Force);
+        }
     }
 }
