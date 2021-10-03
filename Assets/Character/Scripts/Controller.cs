f540da76c14bea26e453085404824e22215fd960
Ephraim Goodenough <ephraim.goodenough@neuromatics.net>
2021-10-03 11:57:33 -0500

    Fixed Character Movement

--- Assets/Character/Scripts/Controller.cs
+++ Assets/Character/Scripts/Controller.cs
@@ -1,35 +1,68 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class Controller : MonoBehaviour
 {
-    public float maxAngularVelocity = 100;
+    public BallController inputActions;
 
+    //public float maxAngularVelocity = 100;
     public float torqueForce = 1;
 
-    private Rigidbody rb;
-    public Rigidbody rigidbody { 
-        get { if (rb == null) rb = transform.GetComponent<Rigidbody>(); return rb; } 
-        set => rb = value;    
+    private bool forward = false, backward = false, right = false, left = false;
+
+    //private Rigidbody rb;
+    new public Rigidbody rigidbody;// { 
+    //    get { if (rb == null) rb = transform.GetComponent<Rigidbody>(); return rb; } 
+    //    set => rb = value;    
+    //}
+
+    private void Awake()
+    {
+        inputActions = new BallController();
+
+        inputActions.Player.Speed.performed += (val) => {
+            forward = (val.ReadValue<float>() > 0);
+            backward = val.ReadValue<float>() < 0;
+        };
+
+        inputActions.Player.Speed.canceled += (val) => {
+            forward = false; backward = false;
+        };
+
+        inputActions.Player.Steering.performed += (val) => {
+            right = val.ReadValue<float>() < 0;
+            left = val.ReadValue<float>() > 0;
+        };
+
+        inputActions.Player.Steering.canceled += (val) => {
+            right = false; left = false;
+        };
+    }
+
+    private void OnEnable() {
+        inputActions.Enable();
+    }
+
+    private void OnDisable() {
+        inputActions.Disable();
     }
 
     void FixedUpdate()
     {
         var torque = rigidbody.angularVelocity;
 
-        if (Input.GetKey(KeyCode.LeftArrow) && torque.z > -maxAngularVelocity) {
-            rigidbody.AddRelativeTorque(new Vector3(0,0,-1) * torqueForce * Time.fixedDeltaTime, ForceMode.Force);
+        if (left) {
+            rigidbody.AddTorque(new Vector3(0,0,-1) * torqueForce * Time.fixedDeltaTime, ForceMode.Impulse);
         }
-        if (Input.GetKey(KeyCode.RightArrow) && torque.z < maxAngularVelocity) {
-            rigidbody.AddRelativeTorque(new Vector3(0,0,1) * torqueForce * Time.fixedDeltaTime, ForceMode.Force);
+        if (right) {
+            rigidbody.AddTorque(new Vector3(0,0,1) * torqueForce * Time.fixedDeltaTime, ForceMode.Impulse);
         }
-        if (Input.GetKey(KeyCode.DownArrow) && torque.x > -maxAngularVelocity) {
-            rigidbody.AddRelativeTorque(new Vector3(-1,0,0) * torqueForce * Time.fixedDeltaTime, ForceMode.Force);
+        if (backward) {
+            rigidbody.AddTorque(new Vector3(-1,0,0) * torqueForce * Time.fixedDeltaTime, ForceMode.Impulse);
         }
-        if (Input.GetKey(KeyCode.UpArrow) && torque.x < maxAngularVelocity) {
-            rigidbody.AddRelativeTorque(new Vector3(1,0,0) * torqueForce * Time.fixedDeltaTime, ForceMode.Force);
+        if (forward) {
+            rigidbody.AddTorque(new Vector3(1,0,0) * torqueForce * Time.fixedDeltaTime, ForceMode.Impulse);
         }
     }
 }
